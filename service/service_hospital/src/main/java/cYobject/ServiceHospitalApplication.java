dd31fa939f9bca6eebd5c1810b918f09a962f654
jax <yefucun@Outlook.com>
2022-05-23 01:04:52 +0800

    init

---
+++ service/service_hospital/src/main/java/cYobject/ServiceHospitalApplication.java
@@ -0,0 +1,14 @@
+package cYobject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+public class ServiceHospitalApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHospitalApplication.class, args);
+    }
+}
